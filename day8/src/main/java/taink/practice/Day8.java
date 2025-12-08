5c75dc0385d5421c943635ffb84fc51935a9b85e
Taink <tainkk@protonmail.com>
2025-12-08 18:34:04 +0100

    chore: setup day 8

---
+++ day8/src/main/java/taink/practice/Day8.java
@@ -0,0 +1,19 @@
+package taink.practice;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Day8 {
+    static void main() {
+        Path path = Paths.get("src/main/resources/input.txt");
+        List<String> input;
+        try {
+            input = Files.readAllLines(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
