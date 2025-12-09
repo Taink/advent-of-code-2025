9992ae2415751a02d619f1035240e3ad80b0925f
Taink <tainkk@protonmail.com>
2025-12-09 01:53:01 +0100

    feat: day 8 part 1

---
+++ common/src/main/java/taink/practice/common/Triple.java
@@ -0,0 +1,4 @@
+package taink.practice.common;
+
+public record Triple<T1, T2, T3>(T1 x, T2 y, T3 z) {
+}
--- day8/src/main/java/taink/practice/Day8.java
+++ day8/src/main/java/taink/practice/Day8.java
@@ -1,5 +1,7 @@
 package taink.practice;
 
+import taink.practice.common.Triple;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,12 +10,77 @@ import java.util.*;
 
 public class Day8 {
     static void main() {
+        int limit = 1000;
         Path path = Paths.get("src/main/resources/input.txt");
+//        int limit = 10;
+//        Path path = Paths.get("src/test/resources/small_input.txt");
         List<String> input;
         try {
             input = Files.readAllLines(path);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        List<Triple<Integer, Integer, Integer>> triples = new ArrayList<>();
+        for (String line : input) {
+            String[] coords = line.split(",");
+            Triple<Integer, Integer, Integer> coordsTriple = new Triple<>(
+                    Integer.parseInt(coords[0]),
+                    Integer.parseInt(coords[1]),
+                    Integer.parseInt(coords[2]));
+            triples.add(coordsTriple);
+        }
+
+        IO.println(triples);
+
+        Map<Set<Triple<Integer, Integer, Integer>>, Integer> distanceMatrix = new HashMap<>();
+        for (int i = 0; i < triples.size() - 1; i++) {
+            var baseTriple = triples.get(i);
+            for (int j = i + 1; j < triples.size(); j++) {
+                var otherTriple = triples.get(j);
+                var pair = Set.of(baseTriple, otherTriple);
+                distanceMatrix.computeIfAbsent(pair, _ -> distance(baseTriple, otherTriple));
+            }
+        }
+
+        IO.println("Computed "+distanceMatrix.size()+" euclidian distances for "+triples.size()+" triples");
+
+        List<Set<Triple<Integer, Integer, Integer>>> circuits = new ArrayList<>();
+        var analyzedDistanceMatrix = distanceMatrix.entrySet().stream().sorted(Comparator.comparingInt(Map.Entry::getValue)).limit(limit).toList();
+        for (var distanceByPair : analyzedDistanceMatrix) {
+            var pair = distanceByPair.getKey().iterator();
+            var first = pair.next();
+            var second = pair.next();
+            Set<Triple<Integer, Integer, Integer>> firstMatch = null;
+            for (Set<Triple<Integer, Integer, Integer>> circuit : List.copyOf(circuits)) {
+                if (circuit.contains(first) || circuit.contains(second)) {
+                    if (firstMatch != null) {
+                        firstMatch.addAll(circuit);
+                        circuits.remove(circuit);
+                    }
+                    circuit.add(first);
+                    circuit.add(second);
+                    firstMatch = circuit;
+                }
+            }
+            if (firstMatch == null) {
+                var newSet = new HashSet<>(distanceByPair.getKey());
+                circuits.add(newSet);
+            }
+        }
+        IO.println(circuits.size() + " circuits with more than 1 junction box found among the lowest "+limit+" distances. Listing:");
+        circuits.forEach(IO::println);
+
+        var circuitsBySizeDesc = circuits.stream().map(Set::size).sorted(Collections.reverseOrder());
+        var sizeProductOfThreeLargestCircuits = circuitsBySizeDesc.limit(3).reduce(1, (acc, b) -> acc * b);
+        IO.println("Multiplying together the sizes of the three largest circuits produces: "+sizeProductOfThreeLargestCircuits);
+    }
+
+    static int distance(Triple<Integer, Integer, Integer> coords1, Triple<Integer, Integer, Integer> coords2) {
+        double result = Math.sqrt(Math.powExact((long) (coords2.x() - coords1.x()), 2) +
+                Math.powExact((long) (coords2.y() - coords1.y()), 2) +
+                Math.powExact((long) (coords2.z() - coords1.z()), 2)
+        );
+        return (int) result;
     }
 }
